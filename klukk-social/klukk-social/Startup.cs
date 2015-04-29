60013e66e8bd7a0dc5f2507e0059c96e1b8c9190
Gummi <gudmundurk14@ru.is>
2015-04-29 20:49:21 +0000

    First post/empty project setup

---
+++ klukk-social/klukk-social/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(klukk_social.Startup))]
+namespace klukk_social
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
