4621b8e940ee2b0a2f71e6cc9018420cca98b93e
Gummi <gudmundurk14@ru.is>
2015-05-06 21:40:31 +0000

    Added controllers and service classes

--- klukk-social/klukk-social/Controllers/HomeController.cs
+++ klukk-social/klukk-social/Controllers/HomeController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using klukk_social.Services;
 
 namespace klukk_social.Controllers
 {
@@ -23,29 +24,8 @@ namespace klukk_social.Controllers
         public ActionResult Contact()
         {
             ViewBag.Message = "Your contact page.";
-            test stuff;
-
-            
 
             return View();
-        }
-    }
-
-    /// <summary>
-    /// Class used to be a test class
-    /// for displaying test things in 
-    /// a test enviroment.
-    /// </summary>
-    public class test
-    {
-        /// <summary>
-        /// This function writes the parameters to the console
-        /// </summary>
-        /// <param name="number"></param>
-        /// <param name="bla"></param>
-        public void DoStuff(int number, string bla)
-        {
-            Console.WriteLine(number + bla);
         }
     }
 }
---
+++ klukk-social/klukk-social/Services/GroupService.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace klukk_social.Services
+{
+    public class GroupService
+    {
+    }
+}
