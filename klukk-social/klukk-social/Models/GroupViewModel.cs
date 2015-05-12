4f53d9e3f784470cba1d0ef95840868d77840572
HeidarBerg <geimkisi@gmail.com>
2015-05-12 15:31:13 +0000

    Group view and code changes for star report

---
+++ klukk-social/klukk-social/Models/GroupViewModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace klukk_social.Models
+{
+	public class GroupViewModel
+	{
+		List<Group> GroupList { get; set; }
+	}
+}
