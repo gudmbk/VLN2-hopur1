2a92e058e67c377fca655bd24ee69b5ddf0a1a6d
Gudridur Sturludottir <gudridurs11@ru.is>
2015-05-13 17:25:40 +0000

    veit ekki hvad annad

---
+++ klukk-social/klukk-social/Models/GroupRequest.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace klukk_social.Models
+{
+    public class GroupRequest
+    {
+        public int Id { get; set; }
+        public string FromUserId { get; set; }
+        [ForeignKey("FromUserId")]
+        public User FromUser { get; set; }
+        public string GroupId { get; set; }
+        [ForeignKey("GroupId")]
+        public Group Togroup { get; set; }
+    }
+}
---
+++ klukk-social/klukk-social/Models/ReportItem.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace klukk_social.Models
+{
+    public class ReportItem
+    {
+        public int Id { get; set; }
+        public bool IsPost { get; set; }
+        public virtual string ParentId { get; set; }
+        public virtual Post PostItem { get; set; }
+        public virtual Comment CommentItem { get; set; }
+        public DateTime Date { get; set; }
+    }
+}
